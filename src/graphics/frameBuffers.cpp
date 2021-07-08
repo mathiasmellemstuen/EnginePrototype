0e6b46198697997ae9c450049441aa80b704f534
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2021-07-09 00:31:12 +0200

    Added framebuffers

---
+++ src/graphics/frameBuffers.cpp
@@ -0,0 +1,44 @@
+#include "frameBuffers.h"
+#include <vulkan/vulkan.h>
+#include <vector>
+#include "imageViews.h"
+#include "logicalDevice.h"
+#include "swapChain.h"
+#include "graphicsPipeline.h"
+#include "../utility/log.h"
+
+FrameBuffers::FrameBuffers(LogicalDevice& logicalDevice, ImageViews& imageViews, SwapChain& swapChain, GraphicsPipeline& graphicsPipeline) {
+
+    log(INFO, "Creating framebuffers"); 
+    this->device = &logicalDevice.device; 
+
+    swapChainFramebuffers.resize(imageViews.swapChainImageViews.size());
+
+    for (size_t i = 0; i < imageViews.swapChainImageViews.size(); i++) {
+        
+        VkImageView attachments[] = {
+            imageViews.swapChainImageViews[i]
+        };
+
+        VkFramebufferCreateInfo framebufferInfo{}; 
+        framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+        framebufferInfo.renderPass = graphicsPipeline.renderPass;
+        framebufferInfo.attachmentCount = 1;
+        framebufferInfo.pAttachments = attachments;
+        framebufferInfo.width = swapChain.swapChainExtent.width;
+        framebufferInfo.height = swapChain.swapChainExtent.height;
+        framebufferInfo.layers = 1;
+        
+        if (vkCreateFramebuffer(*device, &framebufferInfo, nullptr, &swapChainFramebuffers[i]) != VK_SUCCESS) {
+            log(ERROR, "Failed to create framebuffer!"); 
+            throw std::runtime_error("failed to create framebuffer!");
+        }
+    }
+    log(SUCCESS, "Successfully created all framebuffers."); 
+};
+
+FrameBuffers::~FrameBuffers() {
+    for (auto framebuffer : swapChainFramebuffers) {
+        vkDestroyFramebuffer(*device, framebuffer, nullptr); 
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "graphics/shader.h"
 #include "utility/log.h"
 #include "graphics/graphicsPipeline.h"
+#include "graphics/frameBuffers.h"
 
 #include <iostream>
 #include <SDL2/SDL.h>
@@ -25,7 +26,8 @@ int main(int argc, char *argv[]) {
     SwapChain swapChain(physicalDevice, logicalDevice);
     ImageViews ImageViews(swapChain, logicalDevice);
     Shader shader(logicalDevice, "shaders/vert.spv", "shaders/frag.spv");
-    GraphicsPipeline grapicsPipeline(logicalDevice, swapChain, shader);
+    GraphicsPipeline graphicsPipeline(logicalDevice, swapChain, shader);
+    FrameBuffers frameBuffers(logicalDevice, ImageViews, swapChain, graphicsPipeline);
 
     while(window.running) {
         
@@ -35,6 +37,7 @@ int main(int argc, char *argv[]) {
             }
         }
     }
+
     log(INFO, "Exiting application!"); 
     return 0;
 }
