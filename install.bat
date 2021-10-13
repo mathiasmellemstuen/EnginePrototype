16eba7c95a8d2eb0aef0776d82db8d4d523b9f4e
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2021-10-13 19:50:01 +0200

    Install script should work for windows

--- install.bat
+++ install.bat
@@ -1,23 +1,29 @@
-#!/bin/sh
+@echo off 
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
 
 echo "Downloading submodules." 
 git submodule update --init --recursive
 git pull --recurse-submodules
 
 echo "Installing SDL library on your system"
-cd thirdparty/SDL
-./configure
-make
-sudo make install
+cd thirdparty/SDL/VisualC/SDL
+msbuild /P:Configuration=Release /p:Platform=x64 SDL.vcxproj
+cd x64/Release/
+copy SDL2.lib "../../../../../../SDL2.lib"
+copy SDL2.dll "../../../../../../SDL2.dll"
+cd ../../../SDLmain/
+msbuild /P:Configuration=Release /p:Platform=x64 SDLmain.vcxproj
+cd x64/Release/
+copy SDL2main.lib "../../../../../../SDL2main.lib"
 
 echo "Setting up imgui"
-cd ../imgui/imgui
-cp backends/imgui_impl_sdl.cpp imgui_impl_sdl.cpp
-cp backends/imgui_impl_sdl.h imgui_impl_sdl.h
-cp backends/imgui_impl_vulkan.cpp imgui_impl_vulkan.cpp
-cp backends/imgui_impl_vulkan.h imgui_impl_vulkan.h
-rm -r backends/
-rm -r examples/
-rm -r docs/
-rm -r misc/
-
+cd ../../../../../imgui/imgui
+copy "backends/imgui_impl_sdl.cpp" "imgui_impl_sdl.cpp"
+copy "backends/imgui_impl_sdl.h" "imgui_impl_sdl.h"
+copy "backends/imgui_impl_vulkan.cpp" "imgui_impl_vulkan.cpp"
+copy "backends/imgui_impl_vulkan.h" "imgui_impl_vulkan.h"
+Rmdir /S "backends/"
+Rmdir /S "examples/""
+Rmdir /S "docs/""
+Rmdir /S "misc/"
+cd ../../../
