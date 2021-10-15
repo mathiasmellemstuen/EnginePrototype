90c512c94e045b60e605e6b798582741716a73cc
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2021-10-15 17:27:27 +0200

    Minor fix in compile script for windows.

--- compileAndRunWindows.bat
+++ compileAndRunWindows.bat
@@ -1,4 +1,4 @@
-C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
 cd build/
 cmake ..
 cmake --build . --target ENGINEPROTOTYPE --config Release
