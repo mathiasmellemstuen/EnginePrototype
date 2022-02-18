ec8f51077d46b43256900b3f7f2370e2ccb77ad7
Tobias Hallingstad <tobias@hallingstad.me>
2022-02-18 21:45:54 +0100

    More work on the DAE parsing

--- src/graphics/model.cpp
+++ src/graphics/model.cpp
@@ -5,6 +5,7 @@
 #include <rapidxml/rapidxml_utils.hpp>
 #include <stdexcept>
 #include <unordered_map>
+#include "face.h"
 
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/hash.hpp>
@@ -89,28 +90,92 @@ void Model::loadDAE(const std::string &modelPath) {
     strcpy(tab2, str.c_str());
 
     rapidxml::xml_document<> doc;
-    doc.parse<0>(tab2);
+    rapidxml::xml_node<> *rootNode = nullptr;
 
-    rapidxml::xml_node<> *pRoot = doc.first_node();
-    if (pRoot == nullptr) {
-        std::cout << "Error with root node" << std::endl;
-    } else {
-        std::cout << "Name: " << pRoot->name() << std::endl;
+
+
+    // Parse the file
+    doc.parse<rapidxml::parse_declaration_node | rapidxml::parse_no_data_nodes>(tab2);
+
+    rootNode = doc.first_node("COLLADA");
+    if (rootNode == nullptr)
+        std::cout << "rootNode is null" << std::endl;
+
+    // Get correct node to read the data
+    rapidxml::xml_node<> *workingNode = rootNode->first_node("library_geometries")->first_node("geometry")->first_node("mesh");
+    if (workingNode == nullptr)
+        std::cout << "workingNode is null" << std::endl;
+    else
+        std::cout << "Node val: " << workingNode->name() << std::endl;
+
+    // Iterate over source
+    for (rapidxml::xml_node<> *sourceNode = workingNode->first_node("source"); sourceNode; sourceNode = sourceNode->next_sibling()) {
+        rapidxml::xml_attribute<> *currentAtter = sourceNode->first_attribute();
+        rapidxml::xml_node<> *currentNode = sourceNode->first_node();
+
+        // std::cout << "node atter name: " << sourceNode->first_attribute()->value() << std::endl;
+        // std::cout << "sourceNode type: " << sourceNode->first_node()->value() << std::endl;
+
+        std::string atVal = currentAtter->value();
+
+        if (atVal == "Cube-mesh-map-0") {
+            std::string map = currentNode->value();
+
+            std::cout << map << std::endl;
+
+            // Split the value string into a vector string
+            std::stringstream ss(map);
+            std::istream_iterator<std::string> begin(ss);
+            std::istream_iterator<std::string> end;
+            std::vector<std::string> vert(begin, end);
+
+
+        }
+
     }
 
-    rapidxml::xml_node<> *libGeometries = pRoot->first_node("library_geometries");
-    if (libGeometries == nullptr) {
-        std::cout << "Error with lib" << std::endl;
-    } else {
-        std::cout << libGeometries->name() << std::endl;
+    // Loading faces from triangles in the xml
+    rapidxml::xml_node<> *triangles = workingNode->first_node("triangles");
+    if (triangles == nullptr)
+        std::cout << "triangles is null" << std::endl;
+    else
+        std::cout << "triangles: " << triangles->value() << std::endl;
+
+    rapidxml::xml_node<> *p = triangles->first_node("p");
+    if (p == nullptr)
+        std::cout << "p is null" << std::endl;
+    else
+        std::cout << "p: " << p->value() << std::endl;
+
+    std::stringstream ss(p->value());
+    std::istream_iterator<std::string> begin(ss);
+    std::istream_iterator<std::string> end;
+    std::vector<std::string> pStr(begin, end);
+
+    rapidxml::xml_attribute<> *countAt = triangles->first_attribute("count");
+    // std::vector<Face> faces(std::stoi(countAt->value()));
+    std::vector<Face> faces;
+
+    // Add all lines to faces
+    for (int i = 0; i < pStr.size() - 9; i += 9) {
+        Face face;
+        face.v = glm::vec3(std::stoi(pStr[i]), std::stoi(pStr[3+i]), std::stoi(pStr[6+i]));
+        face.n = glm::vec3(std::stoi(pStr[i+1]), std::stoi(pStr[3+i+1]), std::stoi(pStr[6+i+1]));
+        face.t = glm::vec3(std::stoi(pStr[i+2]), std::stoi(pStr[3+i+2]), std::stoi(pStr[6+i+2]));
+        // face.print();
     }
 
-    rapidxml::xml_node<> *mesh = libGeometries->first_node("geometry");
-    if (mesh == nullptr) {
-        std::cout << "Error with mesh" << std::endl;
-    } else {
-        std::cout << "Mesh: " << libGeometries->name() << std::endl;
-    }
+    // Parse all faces to vertex
+    for (const auto& face : faces) {
+        Vertex vertex{};
 
+        vertex.pos = {
+
+        };
+
+        vertex.texCoord = {
+
+        };
+    }
 
 }
