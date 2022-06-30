131233f8f5d2687249f8595ce41b2150b6913c68
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2022-07-01 00:46:31 +0200

    Expanded logging code to log other types than char* and std::string

--- src/utility/logging.cpp
+++ src/utility/logging.cpp
@@ -1,6 +1,7 @@
 #include "logging.h"
-
-void logToTerminal(const char* fileName, const char* functionName, int lineNumber, int logLevel, std::string message) {
+#include <glm/glm.hpp>
+#include <glm/gtx/string_cast.hpp>
+void logToTerminal(const char* fileName, const char* functionName, const int& lineNumber, const int& logLevel, const std::string& message) {
 
     std::string formattedFileName = fileName;
     
@@ -34,3 +35,32 @@ void logToTerminal(const char* fileName, const char* functionName, int lineNumbe
     }
     std::cout << "] " << message << std::endl;
 };
+
+
+void logToTerminal(const char* fileName, const char* functionName, const int& lineNumber, const int& logLevel, const int& message) {
+    logToTerminal(fileName, functionName, lineNumber, logLevel, std::to_string(message)); 
+};
+void logToTerminal(const char* fileName, const char* functionName, const int& lineNumber, const int& logLevel, const float& message) {
+    logToTerminal(fileName, functionName, lineNumber, logLevel, std::to_string(message)); 
+};
+void logToTerminal(const char* fileName, const char* functionName, const int& lineNumber, const int& logLevel, const double& message) {
+    logToTerminal(fileName, functionName, lineNumber, logLevel, std::to_string(message)); 
+};
+void logToTerminal(const char* fileName, const char* functionName, const int& lineNumber, const int& logLevel, const glm::vec2& message) {
+    logToTerminal(fileName, functionName, lineNumber, logLevel, glm::to_string(message)); 
+};
+void logToTerminal(const char* fileName, const char* functionName, const int& lineNumber, const int& logLevel, const glm::vec3& message) {
+    logToTerminal(fileName, functionName, lineNumber, logLevel, glm::to_string(message)); 
+};
+void logToTerminal(const char* fileName, const char* functionName, const int& lineNumber, const int& logLevel, const glm::vec4& message) {
+    logToTerminal(fileName, functionName, lineNumber, logLevel, glm::to_string(message)); 
+}
+void logToTerminal(const char* fileName, const char* functionName, const int& lineNumber, const int& logLevel, const glm::mat2& message) {
+    logToTerminal(fileName, functionName, lineNumber, logLevel, glm::to_string(message)); 
+}
+void logToTerminal(const char* fileName, const char* functionName, const int& lineNumber, const int& logLevel, const glm::mat3& message) {
+    logToTerminal(fileName, functionName, lineNumber, logLevel, glm::to_string(message)); 
+}
+void logToTerminal(const char* fileName, const char* functionName, const int& lineNumber, const int& logLevel, const glm::mat4& message) {
+    logToTerminal(fileName, functionName, lineNumber, logLevel, glm::to_string(message)); 
+}
--- src/utility/logging.h
+++ src/utility/logging.h
@@ -2,6 +2,7 @@
 #define ENGINEPROTOTYPE_LOGGING
 
 #include <string>
+#include <glm/glm.hpp>
 
 enum LogLevel {
     INFO = 1,
@@ -13,7 +14,16 @@ enum LogLevel {
 #define _WIN32_WINNT 0x0A000007
 #include <rang/rang.hpp>
 
-void logToTerminal(const char* fileName, const char* functionName, int lineNumber, int logLevel, std::string message);
+void logToTerminal(const char* fileName, const char* functionName, const int& lineNumber, const int& logLevel, const std::string& message);
+void logToTerminal(const char* fileName, const char* functionName, const int& lineNumber, const int& logLevel, const int& message);
+void logToTerminal(const char* fileName, const char* functionName, const int& lineNumber, const int& logLevel, const float& message);
+void logToTerminal(const char* fileName, const char* functionName, const int& lineNumber, const int& logLevel, const double& message);
+void logToTerminal(const char* fileName, const char* functionName, const int& lineNumber, const int& logLevel, const glm::vec2& message);
+void logToTerminal(const char* fileName, const char* functionName, const int& lineNumber, const int& logLevel, const glm::vec3& message);
+void logToTerminal(const char* fileName, const char* functionName, const int& lineNumber, const int& logLevel, const glm::vec4& message);
+void logToTerminal(const char* fileName, const char* functionName, const int& lineNumber, const int& logLevel, const glm::mat2& message);
+void logToTerminal(const char* fileName, const char* functionName, const int& lineNumber, const int& logLevel, const glm::mat3& message);
+void logToTerminal(const char* fileName, const char* functionName, const int& lineNumber, const int& logLevel, const glm::mat4& message);
 #define logger(l, m) logToTerminal(__FILE__, __func__, __LINE__, l, m)
 
 #endif
