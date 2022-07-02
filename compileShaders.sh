b080903bc42f00710b6213402f621d5770e7929e
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2022-07-02 13:02:02 +0200

    Adding shaders to compile in .sh script

--- compileShaders.sh
+++ compileShaders.sh
@@ -5,4 +5,8 @@ cd assets/shaders/
 /Users/mathias/VulkanSDK/1.2.189.0/macos/bin/glslc uiShader.frag -o compiled/uiShader.frag.spv
 /Users/mathias/VulkanSDK/1.2.189.0/macos/bin/glslc colorShader.vert -o compiled/colorShader.vert.spv
 /Users/mathias/VulkanSDK/1.2.189.0/macos/bin/glslc colorShader.frag -o compiled/colorShader.frag.spv
+/Users/mathias/VulkanSDK/1.2.189.0/macos/bin/glslc uiCircle.vert -o compiled/uiCircle.vert.spv
+/Users/mathias/VulkanSDK/1.2.189.0/macos/bin/glslc uiCircle.frag -o compiled/uiCircle.frag.spv
+/Users/mathias/VulkanSDK/1.2.189.0/macos/bin/glslc uiText.vert -o compiled/uiText.vert.spv
+/Users/mathias/VulkanSDK/1.2.189.0/macos/bin/glslc uiText.frag -o compiled/uiText.frag.spv
 cd ../../
