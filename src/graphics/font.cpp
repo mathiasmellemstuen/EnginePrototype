5df26fd19cbeb5f86a165c3062156395ae60b184
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2022-07-03 15:21:54 +0200

    Untracking unnecesarry files. Fixing validation error in font.cpp

--- src/graphics/font.cpp
+++ src/graphics/font.cpp
@@ -96,8 +96,9 @@ Font& createFont(Renderer& renderer, const std::string& fontPath, int pixelSize)
     uint32_t mipLevels = 1; 
     createImage(renderer, imageWidth, imageHeight, mipLevels, VK_SAMPLE_COUNT_1_BIT, VK_FORMAT_R8_UNORM, VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, font->texture.textureImage, font->texture.textureImageMemory);
 
-    transitionImageLayout(renderer, font->texture.textureImage, VK_FORMAT_R8_UNORM, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL, mipLevels);
+    transitionImageLayout(renderer, font->texture.textureImage, VK_FORMAT_R8_UNORM, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, mipLevels);
     copyBufferToImage(renderer, font->texture.stagingBuffer, font->texture.textureImage, imageWidth, imageHeight);
+    transitionImageLayout(renderer, font->texture.textureImage, VK_FORMAT_R8_UNORM, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL, mipLevels);
 
     vkDestroyBuffer(renderer.device, font->texture.stagingBuffer, nullptr); 
     vkFreeMemory(renderer.device, font->texture.stagingBufferMemory, nullptr); 
